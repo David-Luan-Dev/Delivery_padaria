fda108e351d7b14c3b986154c0bb15182ba6ab0f
DAVID LUAN NUNES BENEDITO <terceiro-dbenedito@icatuseguros.com.br>
2023-01-02 22:38:38 -0300

    criacao da estrutura da aplicacao.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PaginaPrincipalComponent } from './component/Pages/pagina-principal/pagina-principal.component';
+import { LayoutComponent } from './component/template/layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'pagina-principal',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    canActivate: [],
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'pagina-principal',
+        component: PaginaPrincipalComponent
+      },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/component/template/layout/layout.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { MatDividerModule } from '@angular/material/divider';
+
+
+// components
+import { AppComponent } from 'src/app/app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { LayoutComponent } from './layout.component';
+import { SidebarComponent } from './side-bar/sidebar.component';
+
+const material = [
+  MatToolbarModule,
+  MatFormFieldModule,
+  MatIconModule,
+  FormsModule,
+  MatInputModule,
+  MatDividerModule
+]
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    RouterModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    material
+  ],
+  exports: [],
+  declarations: [
+    HeaderComponent,
+    AppComponent,
+    FooterComponent,
+    LayoutComponent,
+    SidebarComponent
+  ],
+  providers: [],
+})
+export class LayoutModule { }
