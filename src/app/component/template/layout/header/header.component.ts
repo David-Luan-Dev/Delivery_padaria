5dce1b33911719c4193ff5f7c4229d354aa25a04
DAVID LUAN NUNES BENEDITO <terceiro-dbenedito@icatuseguros.com.br>
2023-01-04 20:48:18 -0300

    responsividade

--- src/app/component/template/layout/header/header.component.ts
+++ src/app/component/template/layout/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MatDrawer } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  @Input() drawer!: MatDrawer
   value = ""
 }
--- src/app/component/template/layout/side-bar/sidebar.component.ts
+++ src/app/component/template/layout/side-bar/sidebar.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { map, Observable } from 'rxjs';
+
+import {
+  BreakpointObserver,
+  Breakpoints,
+  BreakpointState,
+} from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,5 +13,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent {
+  public isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(map((result: BreakpointState) => result.matches));
+
+  constructor(private breakpointObserver: BreakpointObserver) { }
 
 }
