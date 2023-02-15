def4ff7f6c2b639656683011e02460f3cc299c94
Emiliojcm <121568964+Emiliojcm@users.noreply.github.com>
2023-02-15 17:50:52 -0300

    ajustes na tela de login

--- src/app/component/Pages/padarias/padarias.component.ts
+++ src/app/component/Pages/padarias/padarias.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-padarias',
   templateUrl: './padarias.component.html',
-  styleUrls: ['./padarias.component.css']
+  styleUrls: ['./padarias.component.scss']
 })
 export class PadariasComponent {
 
