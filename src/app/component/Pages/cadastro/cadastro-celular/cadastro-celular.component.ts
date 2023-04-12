f70ec085452f67dbac24700a1ad7d90d4341859e
Emiliojcm <121568964+Emiliojcm@users.noreply.github.com>
2023-04-12 17:03:26 -0300

    tela de cadastro celular com input de localização

--- src/app/component/Pages/cadastro/cadastro-celular/cadastro-celular.component.ts
+++ src/app/component/Pages/cadastro/cadastro-celular/cadastro-celular.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-cadastro-celular',
   templateUrl: './cadastro-celular.component.html',
-  styleUrls: ['./cadastro-celular.component.css']
+  styleUrls: ['./cadastro-celular.component.scss']
 })
 export class CadastroCelularComponent {
   val4!: string;
+  value!: string;
 }
