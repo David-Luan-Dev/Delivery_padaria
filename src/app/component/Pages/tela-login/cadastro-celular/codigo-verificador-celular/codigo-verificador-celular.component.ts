d3947c8a1a86de5785909d6d2f71f943628f7769
Emiliojcm <121568964+Emiliojcm@users.noreply.github.com>
2023-05-29 21:31:00 -0300

    otimizando inputs das dialog material

--- src/app/component/Pages/tela-login/cadastro-celular/codigo-verificador-celular/codigo-verificador-celular.component.ts
+++ src/app/component/Pages/tela-login/cadastro-celular/codigo-verificador-celular/codigo-verificador-celular.component.ts
@@ -1,33 +1,28 @@
-import { Component, ViewChildren, ElementRef, AfterViewInit, QueryList, OnInit } from '@angular/core';
+import { Component, ViewChildren, ElementRef, AfterViewInit, QueryList, OnInit, ViewChild } from '@angular/core';
 
-import { AbstractControl, FormControl, FormGroup, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormControl, FormGroup, FormGroupDirective, NgForm, Validators } from '@angular/forms';
 import {ErrorStateMatcher} from '@angular/material/core';
 import { FormBuilder } from '@angular/forms';
 
-interface Codigo {
-  numero1: string;
-  numero2: string;
-  numero3: string;
-  numero4: string;
-  numero5: string;
-  numero6: string;
-}
 
 @Component({
   selector: 'app-codigo-verificador-celular',
   templateUrl: './codigo-verificador-celular.component.html',
   styleUrls: ['./codigo-verificador-celular.component.scss']
 })
-export class CodigoVerificadorCelularComponent {
+export class CodigoVerificadorCelularComponent implements OnInit{
 
-  form!: FormGroup;
+  CodigoVerificador = this.fb.group({
+    codigo: this.fb.array(['','','','','',''])
+  })
+  numero = this.CodigoVerificador.get('codigo') as FormArray
 
-  codigo: Codigo = {
-    numero1: '', numero2:'', numero3: '',
-    numero4: '', numero5: '', numero6: ''
-  };
+  constructor(private fb: FormBuilder){}
+
+  ngOnInit(): void {
+  }
 
   codigoVerificadorWatsApp() {
-    console.log(this.codigo)
   }
+
 }
